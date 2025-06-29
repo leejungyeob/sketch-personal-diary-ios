ec8c01ec5214998b3756059466b81b7b1f40fe57
DONOTINTO <goods99j@gmail.com>
2025-06-29 16:39:49 +0900

    init: sketch project 생성

---
+++ sketch-personal-diary-ios/sketch-personal-diary-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sketch-personal-diary-ios
+//
+//  Created by 이중엽 on 6/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sketch-personal-diary-ios/sketch-personal-diary-ios/sketch_personal_diary_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sketch_personal_diary_iosApp.swift
+//  sketch-personal-diary-ios
+//
+//  Created by 이중엽 on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct sketch_personal_diary_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
