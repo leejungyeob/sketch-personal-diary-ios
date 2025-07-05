b84d67b0cb67a75e17d28993ef41952ae44331b2
DONOTINTO <goods99j@gmail.com>
2025-07-06 00:46:06 +0900

    ui: DiaryListView 추가 및 적용

--- DiaryLee/DiaryLee/Views/DiaryHomeView.swift
+++ DiaryLee/DiaryLee/Views/DiaryHomeView.swift
@@ -15,19 +15,9 @@ struct DiaryHomeView: View {
         NavigationView {
             
             ScrollView {
-                ForEach(diaryList) { diary in
-                    LazyVStack(alignment: .leading, spacing: 5) {
-                        Text(diary.title)
-                            .font(.headline)
-                        Text(diary.date, style: .date)
-                            .font(.subheadline)
-                            .foregroundStyle(.gray)
-                        Text(diary.content)
-                            .font(.body)
-                            .lineLimit(2)
-                    }
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 10)
+                ForEach($diaryList) { diary in
+                    DiaryListView(diary: diary)
+                        .padding(.top, 10)
                 }
             }
             .padding()
---
+++ DiaryLee/DiaryLee/Views/DiaryListView.swift
@@ -0,0 +1,40 @@
+//
+//  DiaryListView.swift
+//  DiaryLee
+//
+//  Created by 이중엽 on 7/6/25.
+//
+
+import SwiftUI
+
+struct DiaryListView: View {
+    
+    @Binding var diary: Diary
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Text(diary.title)
+                    .font(.title)
+                
+                Spacer()
+                
+                Text(diary.date, style: .date)
+                    .font(.caption)
+            }
+            .padding(.bottom, 10)
+            
+            Text(diary.content)
+                .font(.callout)
+        }
+        .padding(10)
+        .background(.yellow)
+        .cornerRadius(10)
+        .padding(.horizontal, 10)
+        .shadow(color: .black.opacity(0.5), radius: 5)
+    }
+}
+
+#Preview {
+    DiaryListView(diary: .constant(Diary(title: "제목", date: Date(), content: "123")))
+}
